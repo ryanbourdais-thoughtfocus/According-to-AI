730f90e082f2faab2c0b25c5dada1dcfd1909b14
TFCORP\107323 <mohammad.shishakly@thoughtfocus.com>
2024-11-19 19:00:50 -0600

    ATA-4: Update model to 4o

--- process_file_api/__init__.py
+++ process_file_api/__init__.py
@@ -35,7 +35,7 @@ def analyze_text_with_openai(full_text):
         logging.info("Sending transcription to OpenAI for analysis...")
         client = OpenAI(api_key = openai.api_key)
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-4o",
             messages=[
                 {
                     "role": "user",
